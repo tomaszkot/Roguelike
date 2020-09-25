7d7e2d7f2b069661d48dd3a767a70f5bcec0380b
ouadsecond <ouad.second@gmail.com>
2020-09-25 20:29:29 +0200

    hh

---
+++ Roguelike/AttributesHelperExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike
+{
+  public static class AttributesHelperExtension
+  {
+    public static string ToDescription(this Enum enumValue)
+    {
+      var valueString = enumValue.ToString();
+      var res = "";
+
+      int counter = 0;
+      foreach (var ch in valueString)
+      {
+        if (char.IsUpper(ch) && counter > 0)
+        {
+          res += " ";
+        }
+        res += ch;
+
+        counter++;
+      }
+
+      return res;
+    }
+  }
+}
