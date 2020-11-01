84329bd522cacfe815451dd491a21fa618d2878b
ouadsecond <ouad.second@gmail.com>
2020-11-01 10:44:18 +0100

    ss

---
+++ Roguelike/Tiles/Hound.cs
@@ -0,0 +1,41 @@
+﻿using Dungeons.Core;
+using Roguelike.Abstract;
+using Roguelike.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike.Tiles
+{
+  public class TrainedHound : AdvancedLivingEntity, IAlly
+  {
+    public AllyKind Kind { get; set; }
+    public bool Active { get ; set ; }
+
+    public TrainedHound() : base(new Point().Invalid(), '!')
+    {
+      Stats.SetNominal(EntityStatKind.Health, 15);
+      // Character.Mana = 40;
+      var str = 15;
+      Stats.SetNominal(EntityStatKind.Strength, str);//15
+      Stats.SetNominal(EntityStatKind.Attack, str);
+      Stats.SetNominal(EntityStatKind.Magic, 10);
+      Stats.SetNominal(EntityStatKind.Mana, 40);
+      Stats.SetNominal(EntityStatKind.Defense, 10);
+      Stats.SetNominal(EntityStatKind.Dexterity, 10);
+
+      CreateInventory(null);
+
+      Dirty = true;//TODO
+
+      Kind = AllyKind.Dog;
+#if ASCII_BUILD
+        color = ConsoleColor.Yellow;
+#endif
+    }
+
+  }
+}
