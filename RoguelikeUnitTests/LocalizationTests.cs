d1536138afdbc4e797a132d83512ad2a80106281
ouadsecond <49126480+ouadsecond@users.noreply.github.com>
2022-01-01 11:17:44 +0100

    ee

---
+++ RoguelikeUnitTests/LocalizationTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Roguelike.Extensions;
+using Roguelike.History.Hints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RoguelikeUnitTests
+{
+  [TestFixture]
+  class LocalizationTests : TestBase
+  {
+    [Test]
+    public void TestHints()
+    {
+      var hintHistory = new HintHistory();
+
+      var hint = hintHistory.Get(Roguelike.Help.HintKind.LootCollectShortcut);
+      Assert.AreEqual(hint.Info, "Press 'G' to collect a single loot.");
+            
+      hintHistory.SetKeyCode(Roguelike.Help.HintKind.LootCollectShortcut, (int)KeyCode.LeftAlt, (Roguelike.Help.HintKind hk) => { return KeyCode.LeftAlt.ToDescription(); });
+      Assert.AreEqual(hint.Info, "Press 'Left Alt' to collect a single loot.");
+
+      hintHistory.SetKeyCode(Roguelike.Help.HintKind.LootCollectShortcut, (int)KeyCode.A, (Roguelike.Help.HintKind hk) => { return KeyCode.A.ToDescription(); });
+      Assert.AreEqual(hint.Info, "Press 'A' to collect a single loot.");
+
+      hintHistory.SetKeyCode(Roguelike.Help.HintKind.LootCollectShortcut, (int)KeyCode.Alpha1, (Roguelike.Help.HintKind hk) => { return KeyCode.Alpha1.ToDescription(); });
+      Assert.AreEqual(hint.Info, "Press 'Alpha1' to collect a single loot.");
+    }
+  }
+}
