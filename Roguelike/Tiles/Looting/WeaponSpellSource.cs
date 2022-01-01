142172bc1f1f2831b1f1ccb76bcaba1ea0cfb0d7
ouadsecond <49126480+ouadsecond@users.noreply.github.com>
2022-01-01 18:58:15 +0100

    range

---
+++ Roguelike/Tiles/Looting/WeaponSpellSource.cs
@@ -0,0 +1,100 @@
+﻿using Roguelike.Abstract.Projectiles;
+using Roguelike.Abstract.Spells;
+using Roguelike.Spells;
+using Roguelike.Tiles.LivingEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roguelike.Tiles.Looting
+{
+  public class WeaponSpellSource : SpellSource
+  {
+    public Weapon Weapon { get; set; }
+
+    public int Level
+    {
+      get
+      {
+        return this.Weapon.LevelIndex;
+      }
+    }
+    int initChargesCount = 0;
+    public int RestoresCount { get; set; }
+
+    public WeaponSpellSource(Weapon weapon, SpellKind kind, int chargesCount = 15) : base(kind)
+    {
+      this.Weapon = weapon;
+      InitChargesCount = chargesCount;
+    }
+
+    public void Restore()
+    {
+      RestoresCount++;
+      RestoredChargesCount = initChargesCount - 2 * RestoresCount;
+      Count = RestoredChargesCount;
+    }
+
+    public int InitChargesCount
+    {
+      get => initChargesCount;
+      set
+      {
+        initChargesCount = value;
+        Count = value;
+        RestoredChargesCount = Count;
+      }
+    }
+    public int RestoredChargesCount { get; set; }
+
+    public override string GetExtraStatDescriptionFormatted(LivingEntity caller)
+    {
+      var statDescCurrent = GetExtraStatDescription(caller, true);
+      if (statDescCurrent == null)
+        return "";
+      var res = "Level: " + statDescCurrent.Level + "\r\n";
+      var str = string.Join("\r\n", statDescCurrent.GetDescription(false));
+      res += str;
+
+      return res;
+    }
+
+    public override ISpell CreateSpell(LivingEntity caller)
+    {
+      ISpell spell = null;
+      var weapon = Weapon;
+      switch (this.Kind)
+      {
+        case SpellKind.FireBall:
+          spell = new FireBallSpell(caller, weapon);
+          break;
+        case SpellKind.PoisonBall:
+          spell = new PoisonBallSpell(caller, weapon);
+          break;
+        case SpellKind.IceBall:
+          spell = new IceBallSpell(caller, weapon);
+          break;
+      }
+
+      spell = base.CreateSpell(caller);
+
+      if (spell is IProjectile proj)
+      {
+        var rangeInc = 0;
+        if (Level > 1)
+        {
+          if (Level == 2)
+            rangeInc = 1;
+          else
+            rangeInc = Level/2;
+        }
+        proj.Range += rangeInc;
+      }
+
+      return spell;
+    }
+
+  }
+}
