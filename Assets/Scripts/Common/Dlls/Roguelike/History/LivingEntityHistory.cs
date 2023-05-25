df2b147779ec8f31ce297929ad664cbf3769eba5
Koto <tkot@enformatic.com>
2023-05-25 11:24:57 +0200

    fix for game load

--- Assets/Scripts/Common/Dlls/Roguelike/History/LivingEntityHistory.cs
+++ Assets/Scripts/Common/Dlls/Roguelike/History/LivingEntityHistory.cs
@@ -7,46 +7,50 @@ using System.Threading.Tasks;
 
 namespace Roguelike.History
 {
-  public class LivingEntityHistoryItem : HistoryItem
-  {
-    public LivingEntityHistoryItem(LivingEntity dead)
+    public class LivingEntityHistoryItem : HistoryItem
     {
-      Name = dead.name;
-      Tag1 = dead.tag1;
-      Herd = dead.Herd;
-      if(dead is Enemy en)
-        Power = en.PowerKind;
-      Level = dead.Level;
+        public LivingEntityHistoryItem()
+        { 
+        }
+
+        public LivingEntityHistoryItem(LivingEntity dead)
+        {
+            Name = dead.name;
+            Tag1 = dead.tag1;
+            Herd = dead.Herd;
+            if (dead is Enemy en)
+                Power = en.PowerKind;
+            Level = dead.Level;
+        }
+
+        public string Herd { get; set; }
+        public Tiles.LivingEntities.EnemyPowerKind Power { get; set; }
+        public int Level { get; set; }
     }
 
-    public string Herd { get; set; }
-    public Tiles.LivingEntities.EnemyPowerKind Power { get; set; }
-    public int Level { get; set; }
-  }
-
-  public class LivingEntityHistory
-  {
-    public List<LivingEntityHistoryItem> Items { get; set; } = new List<LivingEntityHistoryItem>();
-
-    public int CountByTag1(string tag1)
+    public class LivingEntityHistory
     {
-      return Items.Where(i => i.Tag1 == tag1).Count();
-    }
+        public List<LivingEntityHistoryItem> Items { get; set; } = new List<LivingEntityHistoryItem>();
 
-    public int CountByName(string name)
-    {
-      return Items.Where(i => i.Name == name).Count();
-    }
+        public int CountByTag1(string tag1)
+        {
+            return Items.Where(i => i.Tag1 == tag1).Count();
+        }
 
-    public int CountByHerd(string herd)
-    {
-      return Items.Where(i => i.Herd == herd).Count();
-    }
+        public int CountByName(string name)
+        {
+            return Items.Where(i => i.Name == name).Count();
+        }
 
-    public void AddItem(LivingEntityHistoryItem lh)
-    {
-      Items.Add(lh);
-    }
+        public int CountByHerd(string herd)
+        {
+            return Items.Where(i => i.Herd == herd).Count();
+        }
 
-  }
+        public void AddItem(LivingEntityHistoryItem lh)
+        {
+            Items.Add(lh);
+        }
+
+    }
 }
